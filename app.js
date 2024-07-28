e637791dca54db094ff5d71a7b41ea440becf4e4
Md Armaan <armaanshaikhh786@gmail.com>
2024-07-28 17:57:08 +0530

    use ejs-mate

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 
-app.get("")
 
 
 main()
@@ -22,6 +22,8 @@ app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 // app.get("/testListing",async (req,res) =>{
 //     let sampleListing = new Listing({
@@ -97,7 +99,7 @@ let port = 8080;
 
 
 app.get("/",(req,res)=>{
-    console.log("hi,i am root");
+    res.send("hi,i am root");
 });
 
 app.listen(port,()=>{
