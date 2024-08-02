1621032fe421acd856fd628da1cbf19df870476d
MD ARMAAN <160780557+teslaa143@users.noreply.github.com>
2024-08-02 17:10:59 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -11,8 +11,8 @@ const listingSchema = new schema ({
     description: String,
     image: {
         type: String,
-        default: "https://ai.nero.com/blog/wp-content/uploads/2022/07/Banner-02-1-1560x900.png",
-        set: (v) => v ===""?"https://ai.nero.com/blog/wp-content/uploads/2022/07/Banner-02-1-1560x900.png":v,
+        default: "https://i.pinimg.com/originals/64/1f/29/641f29d2d22a6bd7ec74cb9be185b637.jpg",
+        set: (v) => v ===""?"https://i.pinimg.com/originals/64/1f/29/641f29d2d22a6bd7ec74cb9be185b637.jpg":v,
     },
     price:Number,
     location: String,
