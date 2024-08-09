b2f645f4df9ae654fa019191f432df41b1f68879
Md Armaan <armaanshaikhh786@gmail.com>
2024-08-10 01:24:59 +0530

    display image

--- routes/listing.js
+++ routes/listing.js
@@ -2,40 +2,46 @@ const express = require("express");
 const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
-const {isLoggedIn, isOwner,validateListing} = require("../middleware.js");
+const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
-const multer  = require('multer');
-const {storage} = require("../cloudConfig.js");
+const multer = require("multer");
+const { storage } = require("../cloudConfig.js");
 const upload = multer({ storage });
 
-
-
-  router
+router
   .route("/")
   .get(wrapAsync(listingController.index))
-   .post(isLoggedIn,validateListing,upload.single("listing[image"),wrapAsync(listingController.creatListing)
-);
+  .post(
+    isLoggedIn,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.creatListing )
+  );
 
+  
 
-// New route
-router.get("/new",isLoggedIn,listingController.renderNewForm);
+// new route
+router.get("/new", isLoggedIn, listingController.renderNewForm);
+router.get("/order" , (req,res)=>{res.render("product.ejs")});
 
 router
-.route("/:id")
-.get( wrapAsync(listingController.showListing))
-.put(isLoggedIn,isOwner,validateListing,wrapAsync(listingController.updateListing))
-.delete(isLoggedIn,isOwner,wrapAsync(listingController.destroyListing)
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    upload.single("listing[image]"),
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
+//edit route
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isOwner,
+  wrapAsync(listingController.renderEditForm)
 );
 
-
-
-  
-  
-  //edit route
-  router.get("/:id/edit",isLoggedIn,isOwner,wrapAsync(listingController.renderEditForm));
-  
-  
-
-
-  module.exports = router;
-  
+module.exports = router;
