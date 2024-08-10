a9e8999bf3b8caf0c81871a070c186bb2a226572
Md Armaan <armaanshaikhh786@gmail.com>
2024-08-11 00:30:19 +0530

    Add UI

--- public/js/map.js
+++ public/js/map.js
@@ -3,6 +3,7 @@
 	mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
         container: 'map', // container ID
+        style: "mapbox://styles/mapbox/streets-v12",
         center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
         zoom: 9 // starting zoom
     });
