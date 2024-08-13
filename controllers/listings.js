27d118db200916fb09c66a089de5770368f9e37c
Md Armaan <armaanshaikhh786@gmail.com>
2024-08-13 21:43:00 +0530

    comment section update

--- controllers/listings.js
+++ controllers/listings.js
@@ -72,20 +72,27 @@ module.exports.renderEditForm = async (req,res)=>{
   }; 
 
   module.exports.updateListing = async (req,res)=>{
-    let {id} = req.params ;
-   
-    let listing = await Listing.findByIdAndUpdate(id,{ ...req.body.listing});
-    if (typeof req.file != "undefined"){
-      let url = req.file.path;
-      let filename = req.file.filename;
-      listing.image = {url,filename};
-      await listing.save();
-    }
     
-    req.flash("success","Listing Updated!");
-    res.redirect(`/listings/${id}`);
-  
-  };
+    let response = await geoCodingClient.forwardGeocode({
+      query: req.body.listing.location,
+      limit: 1,
+    }).send();
+      let {id} = req.params ;
+      let listing = await Listing.findByIdAndUpdate(id,{ ...req.body.listing});
+      listing.geometry = response.body.features[0].geometry;
+      await listing.save();
+      
+      if (typeof req.file != "undefined"){
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = {url,filename};
+        await listing.save();
+      }
+      
+      req.flash("success","Listing Updated!");
+      res.redirect(`/listings/${id}`);
+    
+    };
 
   module.exports.destroyListing = async (req,res)=>{
     let  {id} = req.params ;
