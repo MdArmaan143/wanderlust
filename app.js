91c695da6c2dd684dfc3ac9d0fb67981c9427fac
Md Armaan <armaanshaikhh786@gmail.com>
2025-05-17 16:40:00 +0530

    Fix template and authentication issues

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ passport.deserializeUser(User.deserializeUser());
 app.use((req,res,next)=>{
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
-  res.locals.currUser = req.user;
+  res.locals.currUser = req.user || null;
   next();
 
 });
